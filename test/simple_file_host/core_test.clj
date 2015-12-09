4e778e51657fd0195dfcaf67dc97f2d094997eec
jiajun.zhu <jiajun.zhu@upai.com>
2015-12-09 11:14:30 +0800

    init version

---
+++ test/simple_file_host/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-file-host.core-test
+  (:require [clojure.test :refer :all]
+            [simple-file-host.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
