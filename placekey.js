0f11213def776f52c8f93e8fccc86f721a8a7873
jun.lu <jun.lu@husor.com.cn>
2016-01-26 19:51:56 +0800

    go dev

---
+++ placekey.js
@@ -0,0 +1,4 @@
+{
+    'index-top-ios': ['./pages/index.top.ios.js', './assets/index-top-ios.bundle'],//首页头部坑位
+    'index-top-android': ['./pages/index.top.android.js', './assets/index-top-android.bundle'],
+}
---
+++ process.js
@@ -0,0 +1,2 @@
+//根据palcekey.js, create bundle文件, 上传文件到又拍云
+
