e49bb9f9f5f7d4f95750ad16f1fc131e710c3f0b
jun.lu <jun.lu@husor.com.cn>
2016-01-26 21:11:17 +0800

    add process.js&palcekey to create bundle

--- placekey.js
+++ placekey.js
@@ -1,4 +1,4 @@
-{
-    'index-top-ios': ['./pages/index.top.ios.js', './assets/index-top-ios.bundle'],//首页头部坑位
-    'index-top-android': ['./pages/index.top.android.js', './assets/index-top-android.bundle'],
-}
+module.exports = {
+    'index-top-ios': ['./pages/index.top.ios.js', './assets/index-top-ios.bundle','ios'],//首页头部坑位
+    'index-top-android': ['./pages/index.top.android.js', './assets/index-top-android.bundle','android'],
+};
--- process.js
+++ process.js
@@ -1,2 +1,48 @@
+#!/usr/bin/env node
+
 //根据palcekey.js, create bundle文件, 上传文件到又拍云
+var exec = require('child_process').exec,
+    placekey = require('./placekey');
+
+    var process = (function(){
+        var procObj;
+        function findKey(key){
+            for(var pkey in placekey ){
+                if(key === pkey){
+                    procObj = placekey[key];
+                }
+            };
+
+            createBundle(procObj[0], procObj[1], procObj[2])
+        }
+
+        function createBundle(entry, out, plat){
+            // exec('react-native bundle —entry-file '+ entry +
+            //     ' —bundle-output '+ out +' —root —platform '+ plat)
+            var createProcess = exec('react-native bundle --entry-file '+ entry +
+                 ' --bundle-output '+ out +' --root --platform '+ plat, function(err, stdout, stderr) {
+              if (err) throw err;
+              console.log(stdout);
+            });
+
+            createProcess.on('exit', function(){
+                console.log('子进程已关闭');
+                //创建完毕后上传
+                uploadBundle();
+            });
+        }
+        //http upload
+        function uploadBundle(){
+
+        }
+
+        return {
+            createBundle : createBundle,
+            uploadBundle : uploadBundle,
+            findKey : findKey
+        }
+    })();
+//test for fun
+process.findKey('index-top-ios');
+module.exports = process;
 
