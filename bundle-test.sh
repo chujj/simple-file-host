0175e6a8be0a27ca3a5a5dc3e9923ad227ca5197
鹿鸣 <luming.zhu@husor.com>
2016-02-01 18:58:26 +0800

    初始版本-缺少模块

--- bundle-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-react-native bundle --entry-file $1 --bundle-output $2 --root --platform $3
-cp -fp $2 ./assets/index.bundle
-echo 'input 192.168.XXX.XXX:8000, show you the View on your phone'
-(cd assets ; python -m SimpleHTTPServer)
--- bundle.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-react-native bundle --entry-file $1 --bundle-output $2 --root --platform $3
